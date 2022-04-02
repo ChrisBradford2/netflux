b09ab6261dad342a1288c473ab3496d91e437e73
Chris <nico.bar2012@gmail.com>
2022-04-02 19:07:03 +0200

    Issue #1 : Add styles for Header component.

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 className="text-3xl font-bold underline">This is the header</h1>
+            <Image
+                className="object-contain"
+                src="/Netflux.svg"
+                width={200}
+                height={100}
+                alt="Logo"
+            />
+        </header>
+    )
+}
+
+export default Header
