69d73caae13021c1fce5f63e3c3c2420b409afef
Chris <nico.bar2012@gmail.com>
2022-04-03 18:06:52 +0200

    Issue #5 : Fetch API for Categories

---
+++ service/requests.js
@@ -0,0 +1,32 @@
+const API_KEY = process.env.API_KEY;
+
+export default {
+    fetchTrending: {
+        title: 'Trending',
+        url: `/trending/all/week?api_key=${API_KEY}`,
+    },
+    fetchTopRated: {
+        title: 'Top Rated',
+        url: `/movie/top_rated?api_key=${API_KEY}S`,
+    },
+    fetchAction: {
+        title: 'Action',
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=28`,
+    },
+    fetchComedy: {
+        title: 'Comedy',
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=35`,
+    },
+    fetchHorror: {
+        title: 'Horror',
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=27`,
+    },
+    fetchRomance: {
+        title: 'Romance',
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=10749`,
+    },
+    fetchMystery: {
+        title: 'Mystery',
+        url: `/discover/movie?api_key=${API_KEY}&with_genre=9648`,
+    }
+}
