8f190c8307669bc2bfe541b7e5e4105713f63559
Nicolas Barbarisi <56403640+ChrisBradford2@users.noreply.github.com>
2022-04-03 21:55:55 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ import Image from "next/image";
 function Header() {
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
-      <div className='flex flex-grow justify-evenly max-w-2xl'>
+      <div className='flex flex-grow justify-evenly max-w-2md'>
         <HeaderItem title='HOME' Icon={HomeIcon} />
         <HeaderItem title='WISHLIST' Icon={HeartIcon} />
         <HeaderItem title='SEARCH' Icon={SearchIcon} />
