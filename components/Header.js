c817f2b229a73dec451a4e9249a6b1b0c7416906
Nicolas Barbarisi <nico.bar2012@gmail.com>
2022-04-04 16:09:00 +0200

    Add routing to Header

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,7 @@ import {
   SearchIcon,
   UserIcon
 } from "@heroicons/react/outline";
+import Link from 'next/link';
 import HeaderItem from "./HeaderItem";
 import Image from "next/image";
 
@@ -11,10 +12,18 @@ function Header() {
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className='flex flex-grow justify-evenly max-w-md'>
-        <HeaderItem title='HOME' Icon={HomeIcon} />
-        <HeaderItem title='WISHLIST' Icon={HeartIcon} />
-        <HeaderItem title='SEARCH' Icon={SearchIcon} />
-        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+        <Link href="/">
+          <a><HeaderItem title='HOME' Icon={HomeIcon} /></a>
+        </Link>
+        <Link href="/">
+          <a><HeaderItem title='WISHLIST' Icon={HeartIcon} /></a>
+        </Link>
+        <Link href="/">
+          <a><HeaderItem title='SEARCH' Icon={SearchIcon} /></a>
+        </Link>
+        <Link href="/login">
+          <a><HeaderItem title='ACCOUNT' Icon={UserIcon} /></a>
+        </Link>
       </div>
       <Image
         className="object-contain"
