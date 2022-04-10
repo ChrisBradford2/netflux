e6d08468cb0703544d81d03b6efc599fbcfc8925
Restyled.io <commits@restyled.io>
2022-04-10 16:32:46 +0000

    Restyled by clang-format

--- components/Header.js
+++ components/Header.js
@@ -1,38 +1,40 @@
 import {
-  HomeIcon,
   HeartIcon,
+  HomeIcon,
   SearchIcon,
   UserIcon
 } from "@heroicons/react/outline";
-import Link from 'next/link';
-import HeaderItem from "./HeaderItem";
 import Image from "next/image";
+import Link from 'next/link';
+
+import HeaderItem from "./HeaderItem";
 
 function Header() {
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className='flex flex-grow justify-evenly max-w-md'>
         <Link href="/">
-          <a><HeaderItem title='HOME' Icon={HomeIcon} /></a>
+          <a><HeaderItem title='HOME' Icon={
+    HomeIcon} /></a>
         </Link>
         <Link href="/">
-          <a><HeaderItem title='WISHLIST' Icon={HeartIcon} /></a>
+          <a><HeaderItem title='WISHLIST' Icon={
+    HeartIcon} /></a>
         </Link>
         <Link href="/search">
-          <a><HeaderItem title='SEARCH' Icon={SearchIcon} /></a>
+          <a><HeaderItem title='SEARCH' Icon={
+    SearchIcon} /></a>
         </Link>
         <Link href="/login">
-          <a><HeaderItem title='ACCOUNT' Icon={UserIcon} /></a>
+          <a><HeaderItem title='ACCOUNT' Icon={
+    UserIcon} /></a>
         </Link>
       </div>
       <Image
-        className="object-contain"
-        src="/new-netflux.png"
-        width={200}
-        height={100}
-        alt="Logo"
-      />
-    </header>
+  className = "object-contain"
+  src = "/new-netflux.png"
+  width = {200} height = {100} alt =
+      "Logo" / > < /header>
   )
 }
 
--- pages/api/search.js
+++ pages/api/search.js
@@ -1,12 +1,17 @@
 import Results from "../../components/Results";
 import requests from "../../service/requests";
 
-const results = process.env.NODE_ENV === 'production' ? require('../../service/requests').context.query.title : getSortedResultsData()
+const results = process.env.NODE_ENV === 'production'
+                    ? require('../../service/requests').context.query.title
+                    : getSortedResultsData()
 
 export default (req, res) => {
-    const results = req.query.q ?
-    results.filter(results => results.title.toLowerCase().includes(req.query.q)) : []
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ results }))
+  const results =
+      req.query.q
+          ? results.filter(
+                results => results.title.toLowerCase().includes(req.query.q))
+          : []
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({results}))
 }
